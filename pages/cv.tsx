14507dac90f7815cac9702e230e9ad6987d3796b
maxpxtts <maxpetts9@gmail.com>
2021-05-27 22:02:36 +0100

    Initial page creation

---
+++ pages/cv.tsx
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Cv() {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title> Max Petts </title>
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png"
+                />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#93ccdc" />
+                <link rel="shortcut icon" href="/favicon.ico" />
+                <meta name="theme-color" content="#ffffff" />
+            </Head>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>MP CV</h1>
+            </main>
+        </div>
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,20 +6,31 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title> Max Petts </title>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#93ccdc" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
 
-
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to M Pizzles ting
-        </h1>
+        <h1 className={styles.title}>Welcome to M Pizzles ting</h1>
       </main>
     </div>
   );
