a4ce33160be1543e8b859c75907e4be2c2394621
maxpxtts <maxpetts9@gmail.com>
2021-05-27 22:42:41 +0100

    Initial Nav

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,42 @@
+import Link from "next/Link";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Nav />
+      <Component {...pageProps} />
+    </div>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/projects">
+            <a>Projects</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/cv">
+            <a>CV</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </li >
+      </ul >
+    </nav>
+  )
 }
 
 export default MyApp;
