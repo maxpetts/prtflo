e4631a344465fb69cce5df5b3ecf230d6aa9f146
maxpxtts <maxpetts9@gmail.com>
2021-07-19 10:45:11 +0100

    PWA / SEO Optimisation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className={"mainContainer"}>
       <Head>
+        <meta charSet="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+        <meta name="description" content="Max Petts: graduated Bangor University with a 1st in Computer Science. Providing his knowledge for your computational problems" />
+        <meta name="keywords" content="Max Petts, Portfolio, Computer science, Tech Solutions, Birmingham, North Wales, Design, UI Design, UX Design, UI, UX, Programming, Computer Help, Java, JavaScript, C++, Python, HTML, CSS, PHP, Development, Developer, Coder, Code, Backend, Frontend, Full Stack, Git, GitHub, PWA, Web App" />
+        <meta name="author" content="Max Petts" />
         <title> Max Petts </title>
         <link
           rel="apple-touch-icon"
@@ -29,7 +35,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#93ccdc" />
         <link rel="shortcut icon" href="favicon/favicon.ico" />
-        <meta name="theme-color" content="#ffffff" />
+        <meta name="theme-color" content="#8280c7" />
       </Head>
       <Nav />
       <Component {...pageProps} />
