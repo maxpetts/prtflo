6811586ed1e3b95a0e307837b123269f14a810ef
maxpxtts <maxpetts9@gmail.com>
2021-09-28 13:48:19 +0100

    Change to index: "M pizzle" -> "Max Petts"

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to M Pizzles ting</h1>
+        <h1 className={styles.title}>Max Petts</h1>
       </main>
-    </div>
+    </div >
   );
 }
