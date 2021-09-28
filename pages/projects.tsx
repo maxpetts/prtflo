b5c7eb31741554ff2b5024cb0f991dd73366965a
maxpxtts <maxpetts9@gmail.com>
2021-09-28 12:56:10 +0100

    Addition of project JSX

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,13 +1,40 @@
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import { getProjects } from "../lib/projectLoader";
 
-function Projects() {
+function ProjectItem({ slug, title, desc, image, git, complexity, lang }) {
     return (
-        <div className={styles.container}>
-            <main className={styles.main}>
-                <h1 className={styles.title}>Projects</h1>
-            </main>
-        </div>
+        <Link href={slug}>
+            <div className="project">
+                <h2>{title}</h2>
+                <div>
+                    <Image src={image} alt={"a relevant project image"} height={300} width={300} />
+                    <span>{desc}</span>
+                </div>
+            </div>
+        </Link>
+    )
+}
+
+export default function Projects({ projects }) {
+    return (
+        <main className="main">
+            <h1 className="title">Projects</h1>
+            <div className="projectsContainer">
+                {projects.map((item) => (
+                    <ProjectItem key={item.slug} {...item} />
+                ))}
+
+            </div>
+        </main>
     );
 }
 
-export default Projects;
+export async function getStaticProps() {
+    return {
+        props: {
+            projects: getProjects()
+        }
+    }
+}
