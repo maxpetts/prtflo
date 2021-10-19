e2a5241adbfebf3d0e4fd947390c50100a83ce8d
maxpxtts <maxpetts9@gmail.com>
2021-10-19 19:24:50 +0100

    Reformatting due to Prettier

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -4,39 +4,42 @@ import Image from "next/image";
 import { getProjects } from "../lib/projectLoader";
 
 function ProjectItem({ slug, title, desc, image, git, complexity, lang }) {
-    return (
-        <Link href={git}>
-            <div className="project">
-                <h2>{title}</h2>
+  return (
+    <Link href={git}>
+      <div className="project">
+        <h2>{title}</h2>
 
-                <div className="project-desc">
-                <Image src={image} alt={"an image relating to " + title} layout="fill" objectFit="contain" />
-                <span>{desc}</span>
-                </div>
-
-            </div>
-        </Link>
-    )
+        <div className="project-desc">
+          <Image
+            src={image}
+            alt={"an image relating to " + title}
+            layout="fill"
+            objectFit="contain"
+          />
+          <span>{desc}</span>
+        </div>
+      </div>
+    </Link>
+  );
 }
 
 export default function Projects({ projects }) {
-    return (
-        <main className="main">
-            <h1 className="title">Projects</h1>
-            <div className="projectsContainer">
-                {projects.map((item) => (
-                    <ProjectItem key={item.slug} {...item} />
-                ))}
-
-            </div>
-        </main>
-    );
+  return (
+    <main className="main">
+      <h1 className="title">Projects</h1>
+      <div className="projectsContainer">
+        {projects.map((item) => (
+          <ProjectItem key={item.slug} {...item} />
+        ))}
+      </div>
+    </main>
+  );
 }
 
 export async function getStaticProps() {
-    return {
-        props: {
-            projects: getProjects()
-        }
-    }
+  return {
+    props: {
+      projects: getProjects(),
+    },
+  };
 }
