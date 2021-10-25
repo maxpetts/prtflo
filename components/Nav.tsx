f476f9c4167081f615cbd48ba6b1d23ac829a9b1
maxpxtts <maxpetts9@gmail.com>
2021-10-25 16:57:20 +0100

    Prettier reformat

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,7 @@
 /**
  * Returns the basic HTML for the navigation bar.
  * Includes lottie animations for when nav buttons are interacted with.
- * 
+ *
  * @author Max Petts
  * @link https://github.com/maxpxtts/prtflo
  */
@@ -18,65 +18,74 @@ import animation_contact from "../public/icons/contact.json";
  * Component for the individual nav bar buttons,
  * handles lottie animations.
  * Admittedly could've been done as functional but practice makes perfect.
- * 
+ *
  * @param link The desired destination of button, gets passed into the Link component provided by Next.js.
  * @param name The name to be displayed in the button.
  * @param animationPath The path to the Lottie JSON animation.
  */
-class LottieButton extends React.Component<{ link: string, name: string, animationPath: any }> {
-    private container: React.RefObject<HTMLAnchorElement>;
-    private animation: AnimationItem;
-    private isMobile: boolean;
-    public state = { isAnimationPlaying: false };
+class LottieButton extends React.Component<{
+  link: string;
+  name: string;
+  animationPath: any;
+}> {
+  private container: React.RefObject<HTMLAnchorElement>;
+  private animation: AnimationItem;
+  private isMobile: boolean;
+  public state = { isAnimationPlaying: false };
 
-    constructor(props) {
-        super(props);
-        this.state = { isAnimationPlaying: false }
-        // Create reference to container so we can pass it to lotties loadAnimation function
-        this.container = React.createRef<HTMLAnchorElement>();
-        // Ensure event callback function is bound to this class instance because React..
-        this.play_animation = this.play_animation.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = { isAnimationPlaying: false };
+    // Create reference to container so we can pass it to lotties loadAnimation function
+    this.container = React.createRef<HTMLAnchorElement>();
+    // Ensure event callback function is bound to this class instance because React..
+    this.play_animation = this.play_animation.bind(this);
+  }
+
+  componentDidMount() {
+    this.animation = lottie.loadAnimation({
+      container: this.container.current,
+      loop: false,
+      autoplay: false,
+      animationData: this.props.animationPath,
+      name: this.props.name,
+    });
+
+    // Must set isMobile after component mount as navigator isn't created because SSR
+    this.isMobile = navigator.userAgent.includes("Mobile");
+
+    // Event listener added to avoid strange hover upon quick hover successions.
+    this.animation.addEventListener("complete", () =>
+      this.setState({ isAnimationPlaying: false })
+    );
+  }
+
+  componentWillUnmount() {
+    lottie.destroy(this.props.name);
+  }
+
+  play_animation() {
+    if (this.animation !== null && this.state.isAnimationPlaying == false) {
+      this.setState({ isAnimationPlaying: true });
+      this.animation.goToAndPlay(this.animation.firstFrame, true);
     }
+  }
 
-    componentDidMount() {
-        this.animation = lottie.loadAnimation({
-            container: this.container.current,
-            loop: false,
-            autoplay: false,
-            animationData: this.props.animationPath,
-            name: this.props.name
-        });
-
-        // Must set isMobile after component mount as navigator isn't created because SSR
-        this.isMobile = navigator.userAgent.includes("Mobile");
-
-        // Event listener added to avoid strange hover upon quick hover successions.
-        this.animation.addEventListener('complete', () => this.setState({ isAnimationPlaying: false }));
-
-    }
-
-    componentWillUnmount() {
-        lottie.destroy(this.props.name);
-    }
-
-    play_animation() {
-        if (this.animation !== null && this.state.isAnimationPlaying == false) {
-            this.setState({ isAnimationPlaying: true });
-            this.animation.goToAndPlay(this.animation.firstFrame, true);
-        }
-    }
-
-    render() {
-        return (
-            <li>
-                <Link href={this.props.link}>
-                    <a ref={this.container} onMouseOver={this.isMobile ? null : this.play_animation} onClick={this.isMobile ? this.play_animation : null}>
-                        {this.props.name}
-                    </a>
-                </Link>
-            </li >
-        )
-    }
+  render() {
+    return (
+      <li>
+        <Link href={this.props.link}>
+          <a
+            ref={this.container}
+            onMouseOver={this.isMobile ? null : this.play_animation}
+            onClick={this.isMobile ? this.play_animation : null}
+          >
+            {this.props.name}
+          </a>
+        </Link>
+      </li>
+    );
+  }
 }
 
 /**
@@ -86,22 +95,27 @@ class LottieButton extends React.Component<{ link: string, name: string, animati
  * animationPath: The path or JSON object to the animation
  */
 const pages = [
-    { link: "/", name: "Home", animationPath: animation_home },
-    { link: "/projects", name: "Projects", animationPath: animation_projects },
-    { link: "/cv", name: "CV", animationPath: animation_cv },
-    { link: "/contact", name: "Contact", animationPath: animation_contact }
+  { link: "/", name: "Home", animationPath: animation_home },
+  { link: "/projects", name: "Projects", animationPath: animation_projects },
+  { link: "/cv", name: "CV", animationPath: animation_cv },
+  { link: "/contact", name: "Contact", animationPath: animation_contact },
 ];
 
 function Nav() {
-    return (
-        <nav>
-            <ul>
-                {pages.map(({ link, name, animationPath }) => (
-                    <LottieButton key={name} link={link} name={name} animationPath={animationPath}></LottieButton>
-                ))}
-            </ul >
-        </nav >
-    );
+  return (
+    <nav>
+      <ul>
+        {pages.map(({ link, name, animationPath }) => (
+          <LottieButton
+            key={name}
+            link={link}
+            name={name}
+            animationPath={animationPath}
+          ></LottieButton>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
