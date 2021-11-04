ef4049bfee764d9c3337176c3658ca95a3c50421
maxpxtts <maxpetts9@gmail.com>
2021-11-04 15:30:37 +0000

    Update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,23 @@
-import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import React from "react";
 
 export default function Home() {
+
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Max Petts</h1>
-	<p>Please note this site is a work in progress</p>
-      </main>
-    </div >
+    <div>
+      <header>
+        <h1>Max Petts</h1>
+        <p>A thought-provoking punchline</p>
+        <Image src={"/pics/memoji.png"} layout="fill" objectFit="contain" />
+      </header>
+
+      <h1>Please note this site is a work in progress</h1>
+
+      <p>
+        <strong>Proficient in: </strong>
+        Python 3, JS ES6, C++, Java, Swift
+      </p>
+      <main></main>
+    </div>
   );
 }
