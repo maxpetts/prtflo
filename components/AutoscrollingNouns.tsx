e047df00f590889f356a2c75e8fd8f91c6c068f4
maxpxtts <maxpetts9@gmail.com>
2021-11-05 15:58:32 +0000

    Implement scrolling words & CSS fixes (mobile, iPad & desktop)

---
+++ components/AutoscrollingNouns.tsx
@@ -0,0 +1,92 @@
+import React, { CSSProperties } from "react";
+
+// Not sure why I mix functional and class but 🤷‍♀️, deal with it :)
+
+const AutoscrollingNouns = (props) => {
+  const { nouns, ...rest } = props;
+
+  let half = Math.ceil(nouns.length / 2);
+  const top = nouns.splice(0, half);
+  const bottom = nouns.splice(-half);
+
+  // The above math shouldn't fail, but if it does for some reason: display the missed nouns
+  nouns ? console.log("missed nouns : " + nouns) : "";
+
+  const keyframes = `@keyframes nounSlide {
+    from {
+      transform: translateX(0);
+    }
+  
+    to {
+      transform: translateX(-100%);
+    }
+  }`;
+
+  return (
+    <>
+      <style>{keyframes}</style>
+
+      <AnimatedMappedNouns nounsToMap={top} />
+      <AnimatedMappedNouns nounsToMap={bottom} bottom />
+    </>
+  );
+};
+
+class AnimatedMappedNouns extends React.Component<{
+  nounsToMap: string[];
+  bottom?: boolean;
+}> {
+  containerStyle: CSSProperties = {
+    height: "50%",
+    width: "fit-content",
+    minHeight: "2.5rem",
+    whiteSpace: "nowrap",
+    textAlign: "center",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: `${this.props.bottom ? "flex-start" : "flex-end"}`,
+    position: "absolute",
+    // I don't like this syntax; make it better future Max
+    bottom: `${this.props.bottom ? 0 : ""}`,
+    top: `${this.props.bottom ? "" : 0}`,
+  };
+
+  wordsContainerStyle: CSSProperties = {
+    width: "50%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    animation: "nounSlide 17s linear infinite",
+  };
+
+  words: CSSProperties = {
+    margin: "0 1.3rem",
+  };
+
+  render() {
+    return (
+      <div className="nounContainer" style={this.containerStyle}>
+        <div style={this.wordsContainerStyle}>
+          {this.props.nounsToMap.map((noun, index) => {
+            return (
+              <span key={index} style={this.words}>
+                {noun}
+              </span>
+            );
+          })}
+        </div>
+        <div style={this.wordsContainerStyle}>
+          {this.props.nounsToMap.map((noun, index) => {
+            return (
+              <span key={index} style={this.words}>
+                {noun}
+              </span>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AutoscrollingNouns;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,41 @@
 import Image from "next/image";
 import React from "react";
+import AutoscrollingNouns from "../components/AutoscrollingNouns";
 
 export default function Home() {
+  const myNouns = [
+    "UI/UX Designer",
+    "Mountain Biker",
+    "Git Versioner",
+    "Terminal Preferer",
+    "Full Stack Developer",
+    "Linuxer",
+    "Designer",
+    "Software Engineer",
+    "Jailbreaker",
+    "Data Processor",
+    "Vimer",
+    "Detail Stickler",
+    "BMXer",
+    "Coffee Lover",
+    "Can't Giver-uper",
+    "iOS Themer",
+    "Skier",
+    "Mountain Lover",
+    "Bang For Bucker",
+  ];
 
   return (
-    <div>
+    <>
       <header>
         <h1>Max Petts</h1>
-        <p>A thought-provoking punchline</p>
+        <p>👋 A thought-provoking punchline</p>
         <Image src={"/pics/memoji.png"} layout="fill" objectFit="contain" />
       </header>
+
+      <div className="postMastHead">
+        <AutoscrollingNouns nouns={myNouns} />
+      </div>
 
       <h1>Please note this site is a work in progress</h1>
 
@@ -18,6 +44,6 @@ export default function Home() {
         Python 3, JS ES6, C++, Java, Swift
       </p>
       <main></main>
-    </div>
+    </>
   );
 }
